a5b50a58b01e4f3ef4ac30fb0ebf0523780e330d
Tomas <tywe@kth.se>
2023-12-30 15:01:05 +0100

    get score 20

--- minimax_assignment/player.py
+++ minimax_assignment/player.py
@@ -49,72 +49,49 @@ class PlayerControllerMinimax(PlayerController):
 
             # Execute next action
             self.sender({"action": best_move, "search_time": None})
+
+    def h2f_distance(self, fish_position, hook_position):
+            y = abs(fish_position[1] - hook_position[1])
+            dx = abs(fish_position[0] - hook_position[0])
+            x = min(dx, 20 - dx) # 20*20 grid 
         
-    def heuristic_function_gar(self, state):
-        # Fish Proximity
-        fish_positions = state.get_fish_positions()
-        hook_positions = state.get_hook_positions()
-        current_player = state.get_player()
-        fish_values = state.get_fish_scores()
+            return x + y
 
-        fish_proximity_score = 0
-        for fish_number, pos in fish_positions.items():
-            distance_to_hook = abs(pos[0] - hook_positions[current_player][0]) + abs(pos[1] - hook_positions[current_player][1])
-            fish_proximity_score += (1 / (distance_to_hook + 1)) * fish_values[fish_number]
-
-        # Score Differentials
-        player_score, opponent_score = state.get_player_scores()
-        score_differential = player_score - opponent_score
-
-        # Relative Boat Positions
-        relative_position_score = 0
-        if current_player == 0:
-            relative_position_score = hook_positions[0][0] - hook_positions[1][0]
-        else:
-            relative_position_score = hook_positions[1][0] - hook_positions[0][0]
-
-        # Encourage boat movement towards areas with more fish
-        movement_score = 0
-        if not state.get_caught()[current_player]:
-            for fish_number, pos in fish_positions.items():
-                movement_score += (1 / (abs(pos[0] - hook_positions[current_player][0]) + 1)) * fish_values[fish_number]
-
-        # Combine the components with appropriate weights
-        heuristic_score = 0.3 * fish_proximity_score + 0.3 * score_differential + 0.2 * relative_position_score + 0.2 * movement_score
-
-        return heuristic_score
-        
     def heuristic_function(self, state):
-        hook_pos = state.get_hook_positions()
-        green_hook_pos = next(iter(hook_pos.values()))
-        fish_pos = state.get_fish_positions()
-        fish_values = state.get_fish_scores()
-        score_A, score_B = state.get_player_scores()
-        big_score = math.inf * (-1)
-        for fish in fish_pos:
-            fish_coordinates = fish_pos[fish]
-            if abs(green_hook_pos[0] - fish_coordinates[0]) <= 3 and abs(green_hook_pos[1] - fish_coordinates[1]) <= 3:
-                # Nu har vi avgränsat området till ett visst antal fiskar som är närliggande. Nästa steg är att kolla på deras scores:
-                fish_score = fish_values[fish]
-
-                if fish_score > big_score and fish_score > 0:
-                    big_score = fish_score
-
-        score_diff = score_A - score_B
-
-        return max(score_diff, big_score)
-   
+        total_score = state.player_scores[0] - state.player_scores[1]
     
-    def hash_state(self, state):
-        scorex, scorey = state.get_player_scores()
-        caughtx, caughty = state.get_caught()
-        hook_pos = state.get_hook_positions()
-        pos_1 = hook_pos[0]
-        pos_2 = hook_pos[1]
-        
-        return str(scorex)+str(scorey)+str(caughtx)+str(caughty)+str(pos_1)+str(pos_2)
+        h = 0
+        for i in state.fish_positions:
+            fish_positions = state.fish_positions[i]
+            hook_positions = state.hook_positions[0]
 
-    def minimax(self, node, player, depth, alpha, beta, transposition_table, initial_time):
+            y = abs(fish_positions[1] - hook_positions[1])
+            delta_x = abs(fish_positions[0] - hook_positions[0])
+            x = min(delta_x, 20 - delta_x)
+            distance = x + y
+
+            fish_score = state.fish_scores[i]
+            
+            if fish_score > 0 and distance == 0:
+                return float('inf')
+            
+            if fish_score > 0:
+                #exp_distance = math.exp(-distance)
+                h = max(h, fish_score * 1/distance)
+
+        return 2 * total_score + h
+
+    
+    def hash_function(self, state):
+        fish_positions = state.get_fish_positions()
+        fish_scores = state.get_fish_scores()
+
+        pos_dic = {str(pos[0]) + str(pos[1]): score for pos, score in zip(fish_positions.items(), fish_scores.items())}
+
+        return str(state.get_hook_positions()) + str(pos_dic)
+
+
+    def alpha_beta(self, node, player, depth, alpha, beta, transposition_table, initial_time):
         
         if time.time() - initial_time > 0.05:
             raise TimeoutError
@@ -124,27 +101,42 @@ class PlayerControllerMinimax(PlayerController):
             if key in transposition_table and transposition_table[key][0] >= depth:
                 return transposition_table[key][1]  #return heuristic value for that node
             children = node.compute_and_get_children()
+            children.sort(key=lambda child: self.heuristic_function(child.state), reverse=True)
             if len(children) == 0 or depth == 0:
                 bestPossible = self.heuristic_function(node.state)
                   
             elif player == 0:
-                bestPossible = float('inf')*(-1)
+                bestPossible = float('-inf')
                 for child in children:
-                    bestPossible = max(bestPossible, self.minimax(child, 1, depth-1, alpha, beta, transposition_table, initial_time))
+                    bestPossible = max(bestPossible, self.alpha_beta(child, 1, depth-1, alpha, beta, transposition_table, initial_time))
                     alpha = max(alpha, bestPossible)
                     if beta <= alpha:
                         break
             else:
                 bestPossible = float('inf')
                 for child in children:
-                    bestPossible = min(bestPossible, self.minimax(child, 0, depth-1, alpha, beta, transposition_table, initial_time))
+                    bestPossible = min(bestPossible, self.alpha_beta(child, 0, depth-1, alpha, beta, transposition_table, initial_time))
                     beta = min(beta, bestPossible)
                     if beta <= alpha:
                         break
             key = self.hash_state(node.state)
             transposition_table.update({key:[depth, bestPossible]})
-        return bestPossible       
-
+        return bestPossible   
+    
+    def search_best_move(self, node, depth, start_time, transposition_table):
+        alpha = float('-inf')
+        beta = float('inf')
+        
+        children = node.compute_and_get_children()
+        children_scores = []
+        
+        for child in children:
+            score = self.alpha_beta(child, 1, depth, alpha, beta, transposition_table, start_time)
+            children_scores.append(score)
+            
+        best_score_index = children_scores.index(max(children_scores))
+        return children[best_score_index].move
+        
     def search_best_next_move(self, initial_tree_node):
         """
         Use minimax (and extensions) to find best possible next move for player 0 (green boat)
@@ -153,30 +145,20 @@ class PlayerControllerMinimax(PlayerController):
             (see the Node class in game_tree.py for more information!)
         :return: either "stay", "left", "right", "up" or "down"
         :rtype: str
-        """
-
-        first_level = initial_tree_node.compute_and_get_children()
-        alpha = float('inf')*(-1)
-        beta = float('inf')
+        """       
         depth = 0
         start_time = time.time()
-        transposition_table = {}
-        bestChild = None
-        bestResult = float('inf')*(-1)
+        transposition_table = dict()
         timeout = False
-        move = None
+        best_move = 0
         
         while not timeout:
             try:
-                for child in first_level:
-                    result = self.minimax(child, 0, depth, alpha, beta, transposition_table, start_time)
-                    if result > bestResult:
-                        bestResult = result
-                        bestChild = child
+                result = self.search_best_move(initial_tree_node, depth, start_time, transposition_table)
                 depth += 1
+                best_move = result
             except:
                 timeout = True
         #print(ACTION_TO_STR[bestChild.move])
-        if bestChild is not None:
-            move = ACTION_TO_STR[bestChild.move] 
-        return move
+        return ACTION_TO_STR[best_move] 
+        
